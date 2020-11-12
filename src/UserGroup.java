663a82fa020df21bc6b2650e131239c57362a6ae
AlexZ98 <57647433+AlexZ98@users.noreply.github.com>
2020-11-11 21:55:30 -0800

    Update UserGroup.java

--- src/UserGroup.java
+++ src/UserGroup.java
@@ -1,11 +1,21 @@
-public class UserGroup implements UserInterface {
+public class UserGroup implements UserInterface, SysEntry {
     private String userGroupId;
+    private static int numberGroups = 0;
 
     public UserGroup(String userGroupId){
         this.userGroupId = userGroupId;
+        numberGroups++;
     }
 
     public String toString(){
         return userGroupId;
+    }
+    public int getNumberGroups(){
+        return numberGroups;
+    }
+
+    @Override
+    public void accept(SysEntryVisitor sysEntryVisitor) {
+        sysEntryVisitor.visitGroupTotal(this);
     }
 }
