a36ad28869d4ef6195facecf86b54cfe512969f2
AlexZ98 <57647433+AlexZ98@users.noreply.github.com>
2020-11-12 10:57:23 -0800

    Update ShowPositivePercentSysEntryVisitor.java

--- src/ShowPositivePercentSysEntryVisitor.java
+++ src/ShowPositivePercentSysEntryVisitor.java
@@ -5,22 +5,12 @@ import javafx.stage.Stage;
 public class ShowPositivePercentSysEntryVisitor implements SysEntryVisitor {
 
     @Override
-    public void visitUserTotal(User user) {
+    public void visit(UserGroup userGroup) {
 
     }
 
     @Override
-    public void visitGroupTotal(UserGroup userGroup) {
-
-    }
-
-    @Override
-    public void visitMessageTotal(User user) {
-
-    }
-
-    @Override
-    public void visitPositivePercentage(User user) {
+    public void visit(User user) {
         Stage stage = new Stage();
         stage.setTitle("Positive Percent Total");
         stage.setScene(new Scene( new TextField("Percentage of Positive Messages: "+ String.valueOf(user.getPosPercent())), 450, 450));
