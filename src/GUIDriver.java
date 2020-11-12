55295c534a7ba4ed20416234a1f382c6f8d84a01
AlexZ98 <57647433+AlexZ98@users.noreply.github.com>
2020-11-11 21:54:46 -0800

    Update GUIDriver.java

--- src/GUIDriver.java
+++ src/GUIDriver.java
@@ -1,19 +1,18 @@
 import javafx.application.Application;
-
 import javafx.beans.value.ChangeListener;
-
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class GUIDriver extends Application {
 //Main driver class which supports a main method that runs the entire program and the GUI
@@ -24,8 +23,9 @@ public class GUIDriver extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        List<User> listOfUsers = new ArrayList<>();
-        List<UserGroup> listOfGroups = new ArrayList<>();
+        List<String> uniqueUsers = new ArrayList<>();
+        List<User> users = new ArrayList<>();
+        List<UserGroup> groups = new ArrayList<>();
 
         AdminPanel adminPanel = AdminPanel.getInstance();
         adminPanel.getRoot().setExpanded(true);
@@ -34,10 +34,12 @@ public class GUIDriver extends Application {
         StackPane treeContainer = new StackPane();
         treeContainer.getChildren().add(treeView);
 
-        HBox hb1 = new HBox(15, adminPanel.getUserId(), adminPanel.getGroupId(), adminPanel.getShowUserTotal(), adminPanel.getShowMsgTotal());
-        HBox hb2 = new HBox(15, adminPanel.getAddUser(), adminPanel.getAddGroup(),adminPanel.getShowGroupTotal(), adminPanel.getShowPosPercent());
-        HBox hb3 = new HBox(10, adminPanel.getUserTotal(), adminPanel.getGroupTotal(), adminPanel.getMsgTotal(), adminPanel.getPosPercent());
-        VBox vbox = new VBox(20, hb1, adminPanel.getOpenUserView(), hb2, hb3);
+        HBox hb1 = new HBox(15,adminPanel.getUserIdLabel(), adminPanel.getUserId(),adminPanel.getUserGroupLabel(), adminPanel.getGroupId());
+        HBox hb2 = new HBox(15, adminPanel.getAddUser(), adminPanel.getAddGroup());
+        HBox hb3 = new HBox(15, adminPanel.getShowGroupTotal(), adminPanel.getShowPosPercent());
+        HBox hb4 = new HBox(15,adminPanel.getShowUserTotal(), adminPanel.getShowMsgTotal());
+        HBox hb5 = new HBox(15, adminPanel.getOpenUserView());
+        VBox vbox = new VBox(20, hb1, hb2, hb3, hb4, hb5);
         vbox.setAlignment(Pos.TOP_CENTER);
         vbox.setPadding(new Insets(5));
 
@@ -46,58 +48,119 @@ public class GUIDriver extends Application {
         adminPanel.add(treeContainer,0 ,0);
         adminPanel.add(vbox, 1,0);
 
-
         //Lambda function that attaches an event listener to tree items so that when a user presses on one of them an event triggers and runs this code
         treeView.getSelectionModel().selectedItemProperty().addListener((ChangeListener) (observable, oldValue, newValue) -> {
         //In my implementation, I disable the ability to add users or groups until you navigate through a treeview item so that you can add one to the root you are at
-            adminPanel.getAddUser().setDisable(false);
-            adminPanel.getAddGroup().setDisable(false);
             TreeItem<UserInterface> selectedItem = (TreeItem<UserInterface>) newValue;
-            //Scenario in which selected tree item is a leaf AKA student/user
-            //NOTE: Logic applies correctly under the assumption that all necessary additions to the tree are finished
-            //e.g. this logic will result in logic error if a UserGroup has no children, whereas in reality it should have atleast one user.
-            if(selectedItem.getChildren().isEmpty()){
-                if((selectedItem.getChildren().isEmpty() && selectedItem.getParent()==adminPanel.getRoot())){
-                    adminPanel.getUserId().setText(String.valueOf(selectedItem.getValue()));
-                    adminPanel.getGroupId().setText("CPP");
-                }
-                else{
-                    adminPanel.getUserId().setText(String.valueOf(selectedItem.getValue()));
-                    adminPanel.getGroupId().setText(String.valueOf(selectedItem.getParent().getValue()));
-                }
+            TreeItem<UserInterface> previousItem = (TreeItem<UserInterface>) oldValue;
+            //differentiate between users and user groups to get correct info displayed in textfields, and opens up certain features to be accessible by the user
+            if(selectedItem.getValue() instanceof User){
+                adminPanel.getUserId().setText(String.valueOf(selectedItem.getValue()));
+                adminPanel.getGroupId().setText(String.valueOf(selectedItem.getParent().getValue()));
+                adminPanel.getOpenUserView().setDisable(false);
+                adminPanel.getAddUser().setDisable(true);
+                adminPanel.getAddGroup().setDisable(true);
             }
-            //Else we're looking at a root or parent AKA a group
-            else {
+            else if(selectedItem.getValue() instanceof UserGroup){
                 adminPanel.getUserId().setText("");
                 adminPanel.getGroupId().setText(String.valueOf(selectedItem.getValue()));
+                adminPanel.getOpenUserView().setDisable(true);
+                adminPanel.getAddUser().setDisable(false);
+                adminPanel.getAddGroup().setDisable(false);
             }
 
             adminPanel.getAddUser().setOnAction(event -> {
                 TextInputDialog input = new TextInputDialog();
                 input.setTitle("User addition");
+                input.setHeaderText("Adding a new User");
                 input.getDialogPane().setContentText("User Name:");
                 Optional<String> result = input.showAndWait();
                 TextField inputTF = input.getEditor();
-                if(inputTF.getText()!=null && inputTF.getText().length()!=0 && newValue!=null){
-                    adminPanel.createBranch(new User(inputTF.getText()), (TreeItem<UserInterface>)newValue);
+                //Soft input validation, also does not allow adding anything under users only groups, if still attempted nothing will change and program will not produce an error/crash
+                //Also can't add a new user if the given ID was already given to another created user at an earlier point
+                if(!uniqueUsers.contains(inputTF.getText()) && inputTF.getText()!=null && inputTF.getText().length()!=0 && newValue!=null && ((TreeItem<UserInterface>) newValue).getValue() instanceof UserGroup){
                     User newUser = new User(inputTF.getText());
-                    listOfUsers.add(newUser);
+                    uniqueUsers.add(inputTF.getText());
+                    users.add(newUser);
+                    adminPanel.createBranch(newUser, (TreeItem<UserInterface>)newValue);
                 }
             });
             adminPanel.getAddGroup().setOnAction(event -> {
+                TextInputDialog input = new TextInputDialog();
+                input.setTitle("Group Addition");
+                input.setHeaderText("Adding a new Group");
+                input.getDialogPane().setContentText("Group Name:");
+                Optional<String> result = input.showAndWait();
+                TextField inputTF = input.getEditor();
+                //Soft input validation, also does not allow adding anything under users only groups, if still attempted nothing will change and program will not produce an error/crash
+                if(inputTF.getText()!=null && inputTF.getText().length()!=0 && newValue!=null && ((TreeItem<UserInterface>) newValue).getValue() instanceof UserGroup){
+                    UserGroup newUserGroup = new UserGroup(inputTF.getText());
+                    adminPanel.createBranch(newUserGroup, (TreeItem<UserInterface>)newValue, new Circle(5, Color.BLACK));
+                    groups.add(newUserGroup);
+
+                }
+            });
+            adminPanel.getOpenUserView().setOnAction(event -> {
+                Stage stage = new Stage();
+                stage.setTitle("User View");
+                GridPane gridPane = new GridPane();
+                ListView<String> userListView = new ListView<>();
+                userListView.setPrefSize(100,75);
+                userListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+                userListView.getItems().addAll(((TreeItem<User>) newValue).getValue().getFollowerIds());
+                Label followers = new Label("Followers");
+                Button followUserButton = new Button("Follow " + selectedItem.getValue().toString());
+                    followUserButton.setOnAction(event2 -> {
+                        if(previousItem.getValue() instanceof User && selectedItem.getValue() instanceof User && previousItem!=selectedItem && selectedItem!=null && previousItem!=null){
+                            ((User) previousItem.getValue()).followUser((User) selectedItem.getValue(),selectedItem.getValue().toString());
+                            userListView.getItems().add(String.valueOf(((TreeItem<User>) oldValue).getValue()));
+                        };
+                    });
+                ListView<String> newsFeed = new ListView<>();
+                newsFeed.setPrefSize(100,75);
+                ((User)selectedItem.getValue()).getTweetMsgs().forEach((list)->{
+                    newsFeed.getItems().add(String.valueOf(list));
+                });
+                TextField tweet = new TextField();
+                Label news = new Label("Tweets: ");
+                Button postTweet = new Button("Post Tweet");
+                    postTweet.setOnAction(event1 -> {
+                        ((TreeItem<User>)newValue).getValue().Tweet(tweet.getText());
+                        newsFeed.getItems().add(tweet.getText());
+                        tweet.setText("");
+                        ((User) selectedItem.getValue()).update((User)selectedItem.getValue());
+                    });
+
+                gridPane.addRow(0, adminPanel.getUserIdLabel(), adminPanel.getUserId(), followUserButton);
+                gridPane.addRow(1, followers, userListView);
+                gridPane.addRow(2, postTweet, tweet);
+                gridPane.addRow(3, news, newsFeed);
+                gridPane.setVgap(20);
+                gridPane.setHgap(20);
+
+                stage.setScene(new Scene( gridPane, 500, 500));
+                stage.show();
 
             });
 
             });
         adminPanel.getShowGroupTotal().setOnAction(event ->{
-            //Add one because of default group CS3560 that is statically initialized, I do not count the root as a group to be counted.
-            adminPanel.getGroupTotal().setText(String.valueOf(listOfGroups.toArray().length+1));
+            SysEntryVisitor visitor = new ShowGroupTotalSysEntryVisitor();
+            visitor.visitGroupTotal(groups.get(0));
         });
 
         adminPanel.getShowUserTotal().setOnAction(event -> {
-            adminPanel.getUserTotal().setText(String.valueOf(listOfUsers.toArray().length));
+            SysEntryVisitor visitor = new ShowUserTotalSysEntryVisitor();
+            visitor.visitUserTotal(users.get(0));
         });
-
+        adminPanel.getShowPosPercent().setOnAction(event -> {
+            SysEntryVisitor visitor = new ShowPositivePercentSysEntryVisitor();
+            visitor.visitPositivePercentage(users.get(0));
+        });
+        adminPanel.getShowMsgTotal().setOnAction(event -> {
+            SysEntryVisitor visitor  = new ShowMessageTotalSysEntryVisitor();
+            visitor.visitMessageTotal(users.get(0));
+        });
 
 
         Scene myScene = new Scene(adminPanel, 900, 600);
