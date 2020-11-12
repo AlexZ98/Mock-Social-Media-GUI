c9ac04f5b14a56b6c7389edff46028a6a3992f45
AlexZ98 <57647433+AlexZ98@users.noreply.github.com>
2020-11-12 10:56:58 -0800

    Update ShowMessageTotalSysEntryVisitor.java

--- src/ShowMessageTotalSysEntryVisitor.java
+++ src/ShowMessageTotalSysEntryVisitor.java
@@ -5,25 +5,14 @@ import javafx.stage.Stage;
 public class ShowMessageTotalSysEntryVisitor implements SysEntryVisitor {
 
     @Override
-    public void visitUserTotal(User user) {
+    public void visit(UserGroup userGroup){
 
     }
-
     @Override
-    public void visitGroupTotal(UserGroup userGroup) {
-
-    }
-
-    @Override
-    public void visitMessageTotal(User user) {
+    public void visit(User user) {
         Stage stage = new Stage();
         stage.setTitle("Message Total");
         stage.setScene(new Scene( new TextArea("Number of Messages: " + String.valueOf(user.getMsgTotal())), 450, 450));
         stage.show();
-    }
-
-    @Override
-    public void visitPositivePercentage(User user) {
-
     }
 }
