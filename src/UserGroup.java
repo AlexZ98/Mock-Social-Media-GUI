7d36de7833d19fa7ef7346c0625f49cc08b959cb
AlexZ98 <57647433+AlexZ98@users.noreply.github.com>
2020-11-29 13:56:33 -0800

    Update UserGroup.java

--- src/UserGroup.java
+++ src/UserGroup.java
@@ -1,14 +1,21 @@
+import java.util.Date;
+
 public class UserGroup implements UserInterface, SysEntry {
     private String userGroupId;
     private static int numberGroups = 0;
+    private long groupCreated;
 
     public UserGroup(String userGroupId){
         this.userGroupId = userGroupId;
         numberGroups++;
+        groupCreated = System.currentTimeMillis();
     }
 
     public String toString(){
         return userGroupId;
+    }
+    public Date getGroupCreated(){
+        return new Date(groupCreated);
     }
     public int getNumberGroups(){
         return numberGroups;
